Halbaroth/geneweb

commit c48f82a66044965c761af5fe97e94998b1e2a69c

Use File.walk_folder for searching for notes
This PR is a clean-up. The logic for finding notes in the notes_d directory
is simplified using `File.walk_folder`.

lib/gwdb-legacy/database.ml
@@ -1235,21 +1235,14 @@ let with_database ?(read_only = false) bname k =
         close_out oc)
   in
   let ext_files () =
-    let top = Filename.concat bname "notes_d" in
-    let rec loop list subdir =
-      let dir = Filename.concat top subdir in
-      try
-        let files = Sys.readdir dir in
-        Array.fold_left
-          (fun files file ->
-            let f = Filename.concat subdir file in
-            if Filename.check_suffix f ".txt" then
-              Filename.chop_suffix f ".txt" :: files
-            else loop files f)
-          list files
-      with Sys_error _ -> list
-    in
-    loop [] Filename.current_dir_name
+    File.walk_folder ~recursive:true
+      (fun fl files ->
+        match fl with
+        | `File f when Filename.check_suffix f ".txt" ->
+            Filename.chop_suffix f ".txt" :: files
+        | `File _ | `Dir _ -> files)
+      (Filename.concat bname "nodes_d")
+      []
   in
   let bnotes = { nread = read_notes; norigin_file; efiles = ext_files } in
   let base_data =

commit 4d57bd5e26afbd300f6d0332bfa8ec9d9b1b5a0d

Always create `notes_d` and `notes` with Gwdb_driver.make
The script `Gwdb_driver.make`, creating databases, does not creates
all the subdirectories. It is simpler to always create `notes_d` and
`notes`, even if there is no notes in the database.

The detailed list of the changes:
- Create notes and notes_d in `Gwdb_driver.make`. In particular,
  `gwc` will always create them. We cannot assume that old databases
  contain them but we could print a warning message in that case.
- Fix a typo introduced in #2105, the subdirectory is `notes_d`, not
  `nodes_t`!!
- Add a function `copy_file` into `File` module to copy files. Strangly, this
  function is not a part of the stdlib of OCaml.
- Use `File.copy_file` function to copy notes and notes_d in `Outbase.output`.
- Change the buffer size in `My_gzip` because it was a bit small for
  modern computers.
- Rename `File` into `Filesystem` because the module manipulates both
  files and directories.

bin/cache_files/cache_files.ml
@@ -21,7 +21,7 @@ let ( // ) = Filename.concat
 (* Attention: cache files are reorg independant *)
 let set_cache_dir bname =
   let cache_dir = Secure.base_dir () // "etc" // bname // "cache" in
-  File.create_dir ~parent:true cache_dir;
+  Filesystem.create_dir ~parent:true cache_dir;
   cache_dir
 
 let write_cache_file bname fname l =

bin/gwd/gwd.ml
@@ -1802,7 +1802,7 @@ let geneweb_server () =
             null_reopen [Unix.O_WRONLY] Unix.stderr
           end
         else exit 0;
-        File.create_dir ~parent:true ~required_perm:0o755 !GWPARAM.cnt_dir
+        Filesystem.create_dir ~parent:true ~required_perm:0o755 !GWPARAM.cnt_dir
     end;
   Wserver.f GwdLog.syslog !selected_addr !selected_port !conn_timeout
     (if Sys.unix then !max_clients else None) connection
@@ -1889,7 +1889,7 @@ let slashify s =
   String.init (String.length s) conv_char
 
 let make_sock_dir x =
-  File.create_dir ~parent:true x;
+  Filesystem.create_dir ~parent:true x;
   if Sys.unix then ()
   else
     begin

bin/gwu/gwuLib.ml
@@ -1544,7 +1544,7 @@ let rs_printf opts s =
 
 let gwu opts isolated base in_dir out_dir src_oc_ht (per_sel, fam_sel) =
   if out_dir <> "" && not (Sys.file_exists out_dir) then
-    File.create_dir ~parent:true out_dir;
+    Filesystem.create_dir ~parent:true out_dir;
   let to_separate = separate base in
   let out_oc_first = ref true in
   let _ofile, oc, close = opts.oc in

lib/gwdb-legacy/database.ml
@@ -1235,13 +1235,13 @@ let with_database ?(read_only = false) bname k =
         close_out oc)
   in
   let ext_files () =
-    File.walk_folder ~recursive:true
+    Filesystem.walk_folder ~recursive:true
       (fun fl files ->
         match fl with
         | `File f when Filename.check_suffix f ".txt" ->
             Filename.chop_suffix f ".txt" :: files
         | `File _ | `Dir _ -> files)
-      (Filename.concat bname "nodes_d")
+      (Filename.concat bname "notes_d")
       []
   in
   let bnotes = { nread = read_notes; norigin_file; efiles = ext_files } in
@@ -1315,6 +1315,8 @@ let make bname particles ((persons, families, strings, bnotes) as _arrays) k =
   let bdir =
     if Filename.check_suffix bname ".gwb" then bname else bname ^ ".gwb"
   in
+  Filesystem.create_dir ~parent:true (bdir // "notes_d");
+  Filesystem.create_file (bdir // "notes");
   let persons, ascends, unions = persons in
   let families, couples, descends = families in
   let data : Dbdisk.base_data =

lib/gwdb-legacy/outbase.ml
@@ -178,6 +178,30 @@ let output_particles_file particles fname =
   List.iter (fun s -> Printf.fprintf oc "%s\n" (Mutil.tr ' ' '_' s)) particles;
   close_out oc
 
+let ( // ) = Filename.concat
+
+(* TODO: these functions should be defined into [Gwdb_driver] but
+   such a definition introduces a cyclic dependency between [Gwdb_driver]
+   and the present module. The [output] function is invoked in
+   [Gwdb_driver.sync] to synchronize the database on the disk after
+   initializing it in memory. *)
+
+let notes_dir base = base.data.bdir // "notes_d"
+let notes_file base = base.data.bdir // "notes"
+let output_notes base dst = Filesystem.copy_file (notes_file base) dst
+
+(* Copy all the notes from "notes_d" of the database [base] into the
+   destination directory [dst_dir]. *)
+let output_notes_d base dst_dir =
+  let l = base.data.bnotes.Def.efiles () in
+  List.iter
+    (fun f ->
+      let src = notes_dir base // (f ^ ".txt") in
+      let dst = dst_dir // (f ^ ".txt") in
+      Filesystem.create_dir ~parent:true @@ Filename.dirname dst;
+      Filesystem.copy_file src dst)
+    l
+
 let output base =
   (* create database directory *)
   let bname = base.data.bdir in
@@ -299,21 +323,8 @@ let output base =
           Gc.compact ());
         trace "create first name index";
         output_first_name_index base tmp_fnames_inx tmp_fnames_dat;
-        let s = base.data.bnotes.Def.nread "" Def.RnAll in
-        (if s = "" then ()
-        else
-          let oc_not = Secure.open_out tmp_notes in
-          output_string oc_not s;
-          close_out oc_not);
-        List.iter
-          (fun f ->
-            let s = base.data.bnotes.Def.nread f Def.RnAll in
-            let fname = Filename.concat tmp_notes_d (f ^ ".txt") in
-            File.create_dir ~parent:true (Filename.dirname fname);
-            let oc = open_out fname in
-            output_string oc s;
-            close_out oc)
-          (List.rev (base.data.bnotes.Def.efiles ()));
+        output_notes base tmp_notes;
+        output_notes_d base tmp_notes_d;
         output_particles_file base.data.particles_txt tmp_particles
       with e ->
         (try close_out oc_inx with _ -> ());

lib/historyDiff.ml
@@ -49,7 +49,7 @@ let create_history_dirs conf fname =
     let dirs =
       [ history_d conf; String.make 1 fname.[0]; String.make 1 fname.[1] ]
     in
-    File.create_dir ~parent:true (List.fold_left Filename.concat "" dirs)
+    Filesystem.create_dir ~parent:true (List.fold_left Filename.concat "" dirs)
 
 (* ************************************************************************ *)
 (*  [Fonc] write_history_file : config -> string -> gen_record -> unit      *)

lib/imageCarrousel.ml
@@ -68,7 +68,7 @@ let move_file_to_save file dir =
   (* previous version iterated on file types *)
   try
     let save_dir = Filename.concat dir "saved" in
-    File.create_dir ~parent:true save_dir;
+    Filesystem.create_dir ~parent:true save_dir;
     let fname = Filename.basename file in
     let orig_file = Filename.concat dir fname in
     let saved_file = Filename.concat save_dir fname in
@@ -326,7 +326,7 @@ let effective_send_ok conf base p file =
   in
   let fname = Image.default_portrait_filename base p in
   let dir = !GWPARAM.images_d conf.bname in
-  File.create_dir ~parent:true dir;
+  Filesystem.create_dir ~parent:true dir;
   let fname =
     Filename.concat dir
       (if mode = "portraits" then fname ^ extension_of_type typ else fname)
@@ -412,7 +412,7 @@ let effective_send_c_ok conf base p file file_name =
     if mode = "portraits" then !GWPARAM.portraits_d conf.bname
     else Filename.concat (!GWPARAM.images_d conf.bname) fname
   in
-  File.create_dir ~parent:true dir;
+  Filesystem.create_dir ~parent:true dir;
   let fname =
     Filename.concat dir
       (if mode = "portraits" then fname ^ extension_of_type typ else file_name)
@@ -425,7 +425,7 @@ let effective_send_c_ok conf base p file file_name =
     | Some (`Url url) -> (
         let fname = Image.default_portrait_filename base p in
         let dir = Filename.concat dir "saved" in
-        File.create_dir ~parent:true dir;
+        Filesystem.create_dir ~parent:true dir;
         let fname = Filename.concat dir fname ^ ".url" in
         try write_file fname url
         with _ ->
@@ -571,7 +571,7 @@ let effective_delete_c_ok conf base p =
     if mode = "portraits" then !GWPARAM.portraits_d conf.bname
     else Filename.concat (!GWPARAM.images_d conf.bname) fname
   in
-  File.create_dir ~parent:true dir;
+  Filesystem.create_dir ~parent:true dir;
   (* TODO verify we dont destroy a saved image
       having the same name as portrait! *)
   if delete then

lib/notes.ml
@@ -274,7 +274,7 @@ let commit_notes conf base fnotes s =
     String.concat Filename.dir_sep
       [ Util.bpath conf.bname; base_notes_dir base; fname ]
   in
-  File.create_dir ~parent:true (Filename.dirname fpath);
+  Filesystem.create_dir ~parent:true (Filename.dirname fpath);
   (try Gwdb.commit_notes base fname s
    with Sys_error m ->
      Hutil.incorrect_request conf ~comment:("explication todo: " ^ m));
@@ -289,7 +289,7 @@ let commit_wiznotes conf base fnotes s =
       (Util.bpath (conf.bname ^ ".gwb"))
       [ base_wiznotes_dir base; fname ]
   in
-  File.create_dir ~parent:true (Filename.dirname fpath);
+  Filesystem.create_dir ~parent:true (Filename.dirname fpath);
   Gwdb.commit_wiznotes base fname s;
   History.record conf base (Def.U_Notes (p_getint conf.env "v", fnotes)) "mn";
   update_notes_links_db base pg s

lib/util/filesystem.ml
@@ -88,3 +88,17 @@ let walk_folder ?(recursive = false) f path acc =
         traverse stack acc
   in
   traverse [ path ] acc
+
+let copy_file src dst =
+  In_channel.with_open_bin src @@ fun ic ->
+  Out_channel.with_open_bin dst @@ fun oc ->
+  let sz = 8192 in
+  let buf = Bytes.create sz in
+  let rec loop () =
+    let r = In_channel.input ic buf 0 sz in
+    if r > 0 then (
+      Out_channel.output oc buf 0 r;
+      loop ())
+    else Out_channel.flush oc
+  in
+  loop ()

lib/util/filesystem.mli
@@ -47,3 +47,9 @@ val walk_folder :
 
     @raise Unix.Unix_error if the function cannot open a file or a
                            subdirectory in [dir]. *)
+
+val copy_file : string -> string -> unit
+(** [copy_file src dst] copies the file [src] into the destination [dst].
+    The file [dst] is created, if it does not exist.
+
+    This function can be used to copy binary files. *)

lib/util/my_gzip.ml
@@ -41,11 +41,12 @@ let find_nextline b s =
 (* Read from the input channel [ic] until a new line is found in the
    buffer. Return the number of read characters. *)
 let read_until_newline ic =
-  let b = Bytes.create 500 in
+  let sz = 8192 in
+  let buf = Bytes.create sz in
   let rec loop acc =
-    let r = Gzip.input ic.gic b 0 500 in
-    Buffer.add_subbytes ic.tail b 0 r;
-    if contains_newline b r || r = 0 then acc + r else loop (acc + r)
+    let r = Gzip.input ic.gic buf 0 sz in
+    Buffer.add_subbytes ic.tail buf 0 r;
+    if contains_newline buf r || r = 0 then acc + r else loop (acc + r)
   in
   loop 0
 