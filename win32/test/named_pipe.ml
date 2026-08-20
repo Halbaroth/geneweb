dd4ef948c23cc34f35bf7fe9ef8f464cf5429700
Pierre Villemot <pierre.villemot@ocamlpro.com>
2026-08-20 11:38:47 +0200

    WIP

--- win32/test/named_pipe.ml
+++ win32/test/named_pipe.ml
@@ -1,17 +1,17 @@
-module Named_pipe = Geneweb_named_pipe
+module Named_pipe = Geneweb_win32.Named_pipe
 
 type t = {
   msg : string;
   priority : int
 }
 
-let pipe_name = "foo" 
+let pipe_name = "foo"
 
 let client () =
   Format.eprintf "[client] started@.";
   Format.eprintf "[client] waiting pipe...@.";
   Named_pipe.wait pipe_name;
-  let fd = Unix.openfile "\\\\.\\pipe\\foo" [ Unix.O_RDWR ] 0 in 
+  let fd = Unix.openfile "\\\\.\\pipe\\foo" [ Unix.O_RDWR ] 0 in
   let oc = Unix.out_channel_of_descr fd in
   Fun.protect ~finally:(fun () -> close_out_noerr oc) @@ fun () ->
   Marshal.to_channel oc { msg = "Hello"; priority = 18 } [ No_sharing ];
@@ -21,12 +21,12 @@ let spawn_client () =
   let env = Array.append
     [| "IS_CLIENT=yes" |] (Unix.environment ())
   in
-  let _ : int = 
-    Unix.create_process_env Sys.executable_name Sys.argv env Unix.stdin Unix.stdout Unix.stderr 
+  let _ : int =
+    Unix.create_process_env Sys.executable_name Sys.argv env Unix.stdin Unix.stdout Unix.stderr
   in
   ()
 
-let server () = 
+let server () =
   Format.eprintf "[server] started@.";
   let pipe = Named_pipe.open_pipe pipe_name in
   Format.eprintf "[server] pipe created@.";
@@ -39,6 +39,6 @@ let server () =
   Format.eprintf "[server] msg = %S, priority = %d@." msg priority
 
 let () =
-  match Sys.getenv "IS_CLIENT" with 
+  match Sys.getenv "IS_CLIENT" with
   | exception Not_found -> server ()
   | _ -> client ()
